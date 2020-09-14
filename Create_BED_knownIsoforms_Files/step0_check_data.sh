60f8d99295741f1faad9c57a31f3572435381b75
hheiling <hheiling@live.unc.edu>
2020-09-14 15:34:26 -0400

    Use _prepare_gene_anno instead of Create_BED_knownIsoforms

--- Create_BED_knownIsoforms_Files/step0_check_data.sh
+++
@@ -1,5 +0,0 @@
-cut -f 3 Mus_musculus.NCBIM37.67.gtf | sort | uniq -c > "step0_check_data.log"
-
-cut -f 1 Mus_musculus.NCBIM37.67.gtf | sort | uniq -c >> "step0_check_data.log"
-
-# cut -f 1 Mus_musculus.NCBIM37.67.updated.gtf | sort | uniq -c
